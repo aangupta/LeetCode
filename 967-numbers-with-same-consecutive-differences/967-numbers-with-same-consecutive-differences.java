429a6f30a1434699ddb05fe8ac73b765364180a1
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2022-09-03 19:47:26 +0530

    Update 967-numbers-with-same-consecutive-differences.java

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -2,6 +2,10 @@ class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
         List<Integer> numberList = new ArrayList<>();
         
+        //special case
+        if(n == 1)
+            numberList.add(0);
+        
         numsSameConsecDiff(n, k, numberList, 0);
         
         int[] result = new int[numberList.size()];
@@ -24,11 +28,14 @@ class Solution {
             
             if(i == 0 && currentNumber == 0){ //skipping the scenario of leading zeros
                 continue;
-            } else if(i != 0 && currentNumber == 0){
+            } else if(i != 0 && currentNumber == 0){  //case where the currentNumber is zero
                 numsSameConsecDiff(n-1, k, numberList, currentNumber * 10 + i);
-            } else if(Math.abs(currentNumber%10 - i) == k) {
+            } else if(Math.abs(currentNumber%10 - i) == k) { ////add the i value at the unit place of the currentNumber (valid case will be when the difference between the unit place of the current number and i is equal to zero)
                  numsSameConsecDiff(n-1, k, numberList, currentNumber * 10 + i);
             }  
         }
     }
 }
+
+TC : O(2^n) + o(n) 
+SC : O(2^n)
