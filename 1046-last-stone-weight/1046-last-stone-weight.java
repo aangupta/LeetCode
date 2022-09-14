9881307798376793485c0746c01e53b5e2477fa9
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2022-09-14 22:38:45 +0530

    Update 1046-last-stone-weight.java

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -21,3 +21,12 @@ class Solution {
         
     }
 }
+
+Put all elements into a priority queue.
+Pop out the two biggest, push back the difference,
+until there are no more two elements left.
+
+
+Complexity
+Time O(NlogN)
+Space O(N)
