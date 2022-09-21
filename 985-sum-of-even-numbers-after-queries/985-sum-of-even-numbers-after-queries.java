d3c7c6dd6f92cff245ddbcc20925bfadbe0a348a
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2022-09-21 22:10:27 +0530

    Update 985-sum-of-even-numbers-after-queries.java

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -4,7 +4,7 @@ class Solution {
         int[] answer = new int[queries.length];
         
         int evenSum = 0;
-        
+        //calculate the sum of all even numbers
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] %2 == 0)
                 evenSum += nums[i];
@@ -14,12 +14,12 @@ class Solution {
             
             int index = queries[i][1];
             int value = queries[i][0];
-            
+            //if the original nums[index] is even, we need to deduct it from evenSum
             if(nums[index] % 2 == 0)
                 evenSum -= nums[index];
-            
+            //updating the nums[index] value
             nums[index] += value;
-            
+            //if the updated nums[index] is even, we add it the evenSum
             if(nums[index] %2 == 0)
                 evenSum += nums[index];
             
