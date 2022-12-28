15ba2561e66960562deb35eb11c09e8f0b585d34
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2022-12-28 18:25:02 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (92.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,5 @@
 class Solution {
+    //Memoization TC O(n) SC O(n) + O(n) ~ DP array + recurivestack space
     private int maximumSum(int index, int n, int[] nums, int[] dp){
         
         if(index == 0) return nums[0];
@@ -7,19 +8,39 @@ class Solution {
         
         if(dp[index] != -1) return dp[index];
         
+        //robbing the current house
         int robTheCurrentHouse = nums[index] + maximumSum(index-2, n, nums, dp);
+        //not robbing the current house
         int notRobTheCurrentHouse = maximumSum(index-1, n, nums, dp);
         
         return dp[index] = Math.max(robTheCurrentHouse, notRobTheCurrentHouse);
         
     }
     public int rob(int[] nums) {
+        //Tabulation
         int n = nums.length;
-        
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
         
-        return maximumSum(n-1, n, nums, dp);
+        dp[0] = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            //robbing the current house
+            int robTheCurrentHouse = nums[i];
+            if(i > 1)
+                robTheCurrentHouse+= dp[i-2];
+            //not robbing the current house
+            int notRobTheCurrentHouse = dp[i-1];
+            
+            dp[i] = Math.max(robTheCurrentHouse, notRobTheCurrentHouse);
+            
+        }
+        
+        return dp[n-1];
         
     }
 }
+
+//Memoization TC O(n) SC O(n) + O(n) ~ DP array + recurivestack space
+//Tabulation TC O(n) SC O(n) ~ DP array
+
