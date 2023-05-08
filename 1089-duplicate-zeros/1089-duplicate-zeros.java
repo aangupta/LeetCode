59323216d288c3a15f83170a44a7ed8aa59dcc90
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-05-08 20:35:50 +0530

    Time: 1 ms (93.41%), Space: 43 MB (42.61%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,22 +1,26 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                temp.add(0);
-                temp.add(0);
-            }
-            else{
-                temp.add(arr[i]);
-            }
+        int zeroCount = 0;
+        for(int num: arr){
+            if(num == 0)
+                zeroCount++;
         }
         
-        for(int i =0; i < arr.length; i++){
-            arr[i] = temp.get(i);
+        int i = arr.length-1, j = arr.length + zeroCount - 1;
+        
+        while(i != j){
+            insert(arr, i, j--);
+            if(arr[i] == 0){
+                insert(arr, i, j--);
+            }
+            i--;
         }
         
     }
+    
+    private void insert(int[] arr, int i, int j){
+        if(j < arr.length) {
+            arr[j] = arr[i];
+        }
+    }
 }
-
-/*Using extra space*/
