533600a35b901d31d13b06ce241e04512bd91704
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-07-11 15:49:17 +0530

    Time: 1 ms (99.49%), Space: 46.5 MB (5.49%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,28 @@
+class Solution {
+    private void dfs(int node, int[] visited, int[][] isConnected) {
+        visited[node] = 1;
+        
+        for(int i = 0; i < isConnected[node].length; i++){
+            if(isConnected[node][i] == 1 && visited[i] == 0){
+                dfs(i, visited, isConnected);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int noOfProvinces = 0;
+        
+        int vertices = isConnected.length;
+        int visited[] = new int[vertices];
+        
+        for(int i = 0; i < vertices; i++){
+            if(visited[i] == 0){
+                noOfProvinces++;
+                dfs(i, visited, isConnected);
+            }
+        }
+        
+        return noOfProvinces;   
+    }
+}
+
+/*Idea is to find no of connected component*/
