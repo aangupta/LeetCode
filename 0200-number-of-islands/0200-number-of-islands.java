32bc49cbae893b2b53ec9aa6ffec2e650cef8220
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-07-12 16:13:06 +0530

    Time: 5 ms (36.60%), Space: 50.4 MB (25.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,23 +1,47 @@
+class Pair {
+    int row;
+    int col;
+    
+    Pair(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
 class Solution {
     
     int[] rowDir = new int[]{-1, 0, 1, 0};
     int[] colDir = new int[]{0, -1, 0, 1};
     
-    private void dfs(char[][] grid, int currRow, int currCol){
+    private void bfs(char[][] grid, int row, int col){
         int nRows = grid.length;
         int nCols = grid[0].length;
         
-        if(currRow < 0 || currRow >= nRows || currCol < 0 || currCol >= nCols || grid[currRow][currCol] == '0'){
-            return;
+        Queue<Pair> queue = new LinkedList<>();
+        
+        queue.add(new Pair(row, col));
+        grid[row][col] = '0';
+        
+        while(queue.size() > 0){
+            Pair pair = queue.peek();
+            queue.remove();
+            
+            int currRow = pair.row;
+            int currCol = pair.col;
+            
+            for(int i = 0; i < 4; i++){
+                
+                int newRow = currRow + rowDir[i];
+                int newCol = currCol + colDir[i];
+                
+                 if(newRow >= 0 && newRow < nRows && newCol >= 0 && newCol < nCols && grid[newRow][newCol] == '1'){
+                     grid[newRow][newCol] = '0';
+                     queue.add(new Pair(newRow, newCol));
+                 }
+            }
+            
+            
         }
         
-        grid[currRow][currCol] = '0';
-        
-        for(int i = 0; i < 4; i++){
-            int newRow = currRow + rowDir[i];
-            int newCol = currCol + colDir[i];
-            dfs(grid, newRow, newCol);
-        }
         return;
         
     }
@@ -31,7 +55,7 @@ class Solution {
             for(int j = 0; j < nCols; j++){
                 if(grid[i][j] == '1'){
                     count++;
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                 }
             }
         }
