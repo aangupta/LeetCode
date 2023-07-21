d9d9a3d9564f3c55bcd0d66b257b5ac46ae8b308
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-07-21 16:50:02 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (46.40%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -31,6 +31,31 @@ class Solution {
         
         return true;
     }
+    
+    private boolean isBipartiteUsingDFS(int start, int currColor, int[][] graph, int[] color){
+        color[start] = currColor;
+        
+        
+            for(int i = 0; i < graph[start].length; i++){
+                
+                //if adjacent node is not yet color, 
+                //we need to give opposite color to the adjacent node as compared to the current node
+                int neighbour = graph[start][i];
+                
+                if(color[ neighbour] == -1){
+                    if(isBipartiteUsingDFS(neighbour, 1- currColor, graph, color) == false)
+                        return false;
+                }
+                //if adjacent node is of same color as the current node
+                //that means that the someother node might have colored it on some different path
+                else if(color[ neighbour] ==  currColor){
+                    return false;
+                }
+            }
+        
+        return true;
+        
+    }
     public boolean isBipartite(int[][] graph) {
         int vertices = graph.length;
         int[] color = new int[vertices];
@@ -42,7 +67,7 @@ class Solution {
         //checking for all the connected conpnent
         for(int i = 0; i < vertices; i++){
             if(color[i] == -1){
-                if(isBipartite(i, graph, color) == false){
+                if(isBipartiteUsingDFS(i, 0, graph, color) == false){
                     return false;
                 }
             }
