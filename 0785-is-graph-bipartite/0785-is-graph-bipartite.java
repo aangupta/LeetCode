d3303ed1b600041ee9c57012b7333d8b4e494e5d
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-07-21 10:58:34 +0530

    Time: 1 ms (65.46%), Space: 44.1 MB (60.10%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,54 @@
+class Solution {
+    
+    private boolean isBipartite(int start, int[][] graph, int[] color){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        color[start] = 0;
+        
+        while(queue.size() > 0){
+            int node = queue.peek();
+            queue.remove();
+            
+            for(int i = 0; i < graph[node].length; i++){
+                
+                //if adjacent node is not yet color, 
+                //we need to give opposite color to the adjacent node as compared to the current node
+                
+                
+                int neighbour = graph[node][i];
+                
+                if(color[ neighbour] == -1){
+                    color[ neighbour] = 1 - color[node];
+                    queue.add( neighbour);
+                }
+                //if adjacent node is of same color as the current node
+                //that means that the someother node might have colored it on some different path
+                else if(color[ neighbour] == color[node]){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int vertices = graph.length;
+        int[] color = new int[vertices];
+        
+        for(int i = 0; i < vertices; i++){
+            color[i] = -1;
+        }
+        
+        //checking for all the connected conpnent
+        for(int i = 0; i < vertices; i++){
+            if(color[i] == -1){
+                if(isBipartite(i, graph, color) == false){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+        
+    }
+}
