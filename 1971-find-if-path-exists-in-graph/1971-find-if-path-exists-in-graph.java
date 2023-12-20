492fe9094da4f273eef70f73195464a85b768800
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-12-20 23:37:13 +0530

    Time: 116 ms (44.77%), Space: 158.6 MB (37.08%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,16 +1,22 @@
 class Solution {
+    // adjacency list + dfs
+    //TC 0(V+E) 
     
-    private void addEdge(ArrayList<ArrayList<Integer>> adjList, int u, int v){
+    private boolean dfs(int n, ArrayList<ArrayList<Integer>> adjList, int source, int destination, int[] visited) {
         
-        adjList.get(u).add(v);
-        adjList.get(v).add(u);
+        visited[source] = 1;
+        
+        if(source == destination) return true;
+        
+        for(int neighbour : adjList.get(source)) {
+            if(visited[neighbour] == 0 && dfs(n,adjList, neighbour, destination, visited))
+                return true;
+        }
+        
+        return false;
         
     }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        
-        if(n == 1) return true;
-        
-        if(source == destination) return true;
         
         //creating adjList from the given edges array
         ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
@@ -20,38 +26,15 @@ class Solution {
         }
         
         for(int i = 0; i < edges.length; i++){
-            addEdge(adjList, edges[i][0], edges[i][1]);
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
         }
         
         
         int[] visited = new int[n];
         
-        Queue<Integer> queue = new LinkedList<>();
-        
-        //adding the source node to queue and marking it visited
-        queue.add(source);
-        visited[source] = 1;
-        
-        while(queue.size() > 0) {
-            
-            int currNode = queue.peek();
-            queue.remove();
-            
-            //checking all the adj nodes of the current node, if not visited mark it visited and add it to queue
-            for(int adjNode : adjList.get(currNode)) {
-                
-                if(visited[adjNode] == 0) {
-                    
-                    if(adjNode == destination) return true;
-                    
-                    visited[adjNode] = 1;
-                    queue.add(adjNode);
-                }
-            }
-        }
-        
-       
-        return false;
-        
+        return dfs(n, adjList, source, destination, visited);
     }
 }
