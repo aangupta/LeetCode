b2d39488c81eb401d5bdff6a616e7a13ac910ee3
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-12-22 23:52:09 +0530

    Time: 3 ms (45.68%), Space: 43.9 MB (31.10%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,79 +1,54 @@
-class Pair {
-    int row, col;
-    Pair(int row, int col){
-        this.row = row;
-        this.col = col;
-    }
-}
 class Solution {
+    //The catch here is that if the island of 0's is connected to a boundary 0. i.e i =0 || j=0 || i=length-1 || j=length-1 that island will not be considered 
+    //since that would mean that this island is not completely surrounded by water i.e 1's on all sides.
     
-    private void bfs(int row, int col, int[][] grid, int[][] visited) {
+    
+    int[] dx = new int[]{0, -1, 0, 1};
+    int[] dy = new int[]{-1, 0, 1, 0};
+    
+    private void dfs(int row, int col, int[][] grid, int[][] visited) {
         
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int nRows = grid.length;
+        int nCols = grid[0].length;
         
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(row, col));
         visited[row][col] = 1;
-        //grid[row][col] = 1;
         
-        int[] dx = new int[]{0, -1, 0, 1};
-        int[] dy = new int[]{-1, 0, 1, 0};
-        
-        while(queue.size() > 0){
+        for(int i = 0; i < 4; i++) {
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
             
-            int currRow = queue.peek().row;
-            int currCol = queue.peek().col;
-            
-            queue.remove();
-            
-            for(int i = 0; i < 4; i++){
-                
-                int nr = currRow + dx[i];
-                int nc = currCol + dy[i];
-                
-                
-                if(nr >=0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 0 && visited[nr][nc] == 0){
-                    queue.add(new Pair(nr, nc));
-                    visited[nr][nc] = 1;
-                    //grid[nr][nc] = 1;
-                }
-            }
+            if(newRow >= 0 && newRow < nRows && newCol >= 0 && newCol < nCols && grid[newRow][newCol] == 0 && visited[newRow][newCol] == 0)
+                dfs(newRow, newCol, grid, visited);
         }
-        
         
     }
     public int closedIsland(int[][] grid) {
+        int nRows = grid.length;
+        int nCols = grid[0].length;
         
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int[][] visited = new int[nRows][nCols];
         
-        int[][] visited = new int[rows][cols];
-        
-        int count = 0;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                
-                if(i == 0 || i == rows-1 || j == 0 || j == cols-1 ){
+        //marking all boundary land and its connected cell as visited, because it will not contribute to the closed island
+        for(int i = 0; i < nRows; i++){
+            for(int j = 0; j < nCols; j++) {
+                if(i == 0 || i == nRows-1 || j == 0 || j == nCols-1) { //boundary lands
                     if(grid[i][j] == 0 && visited[i][j] == 0){
-                        bfs(i,j,grid, visited);
+                        dfs(i,j,grid,visited);
                     }
                 }
             }
         }
         
         
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+        int count = 0;
+        for(int i = 0; i < nRows; i++){
+            for(int j = 0; j < nCols; j++) {
                 if(grid[i][j] == 0 && visited[i][j] == 0){
-                    bfs(i,j,grid,visited);
-                    count++;
+                    count++; //new closed island
+                    dfs(i,j,grid,visited);
                 }
             }
         }
-        
-        
-        
         
         return count;
         
