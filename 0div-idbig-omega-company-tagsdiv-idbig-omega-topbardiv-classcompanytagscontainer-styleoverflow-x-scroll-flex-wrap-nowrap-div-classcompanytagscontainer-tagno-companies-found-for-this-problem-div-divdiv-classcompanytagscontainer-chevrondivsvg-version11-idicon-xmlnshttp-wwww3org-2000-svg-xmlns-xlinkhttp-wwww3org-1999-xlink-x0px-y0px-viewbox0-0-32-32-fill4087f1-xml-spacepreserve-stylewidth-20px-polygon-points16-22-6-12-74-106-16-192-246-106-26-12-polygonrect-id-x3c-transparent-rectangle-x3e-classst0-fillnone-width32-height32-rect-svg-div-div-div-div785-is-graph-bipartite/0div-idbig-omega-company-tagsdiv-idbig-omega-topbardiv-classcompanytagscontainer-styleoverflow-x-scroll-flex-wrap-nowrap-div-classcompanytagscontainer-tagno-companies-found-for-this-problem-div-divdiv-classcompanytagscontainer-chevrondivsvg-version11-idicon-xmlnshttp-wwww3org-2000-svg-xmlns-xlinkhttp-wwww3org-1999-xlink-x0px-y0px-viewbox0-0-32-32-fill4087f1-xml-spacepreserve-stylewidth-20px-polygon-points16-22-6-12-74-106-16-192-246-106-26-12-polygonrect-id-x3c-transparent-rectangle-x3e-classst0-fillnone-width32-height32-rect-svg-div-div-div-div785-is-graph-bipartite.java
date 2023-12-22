7e5799b7b37ad97c214b640187921a727d43e4a2
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-12-23 00:56:23 +0530

    Time: 1 ms (67.80%), Space: 45.3 MB (6.14%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite.java
@@ -1,26 +1,31 @@
 class Solution {
 
-    private boolean isBipartiteUsingDFS(int start, int currColor, int[][] graph, int[] color) {
+    //BFS
+    private boolean isBipartiteUtils(int start, int currColor, int[][] graph, int[] color) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
         color[start] = currColor;
-
-        for (int i = 0; i < graph[start].length; i++) {
-            //if adjacent node is not yet color,
-            //we need to give opposite color to the adjacent node as compared to the current node
-            int neighbour = graph[start][i];
-
-            if (color[neighbour] == -1) {
-                if (isBipartiteUsingDFS(neighbour, 1 - currColor, graph, color) == false) return false;
-            }
-            //if adjacent node is of same color as the current node
-            //that means that the someother node might have colored it on some different path
-            else if (color[neighbour] == currColor) {
-                return false;
+        
+        while(queue.size() > 0) {
+            int node = queue.peek();
+            queue.remove();
+            
+            for(int neighbour : graph[node]) {
+                //if adjacent node is not yet color,
+                //we need to give opposite color to the adjacent node as compared to the current node
+                if(color[neighbour] == -1) {
+                    color[neighbour] = 1 - color[node];
+                    queue.add(neighbour);
+                }
+                //if adjacent node is of same color as the current node
+                //that means that the someother node might have colored it on some different path
+                else if(color[neighbour] == color[node]) {
+                    return false;
+                }
             }
         }
-
         return true;
     }
-
     public boolean isBipartite(int[][] graph) {
         int vertices = graph.length;
         int[] color = new int[vertices];
@@ -32,7 +37,7 @@ class Solution {
         //checking for all the connected conpnent
         for (int i = 0; i < vertices; i++) {
             if (color[i] == -1) {
-                if (isBipartiteUsingDFS(i, 0, graph, color) == false) {
+                if (isBipartiteUtils(i, 0, graph, color) == false) {
                     return false;
                 }
             }
