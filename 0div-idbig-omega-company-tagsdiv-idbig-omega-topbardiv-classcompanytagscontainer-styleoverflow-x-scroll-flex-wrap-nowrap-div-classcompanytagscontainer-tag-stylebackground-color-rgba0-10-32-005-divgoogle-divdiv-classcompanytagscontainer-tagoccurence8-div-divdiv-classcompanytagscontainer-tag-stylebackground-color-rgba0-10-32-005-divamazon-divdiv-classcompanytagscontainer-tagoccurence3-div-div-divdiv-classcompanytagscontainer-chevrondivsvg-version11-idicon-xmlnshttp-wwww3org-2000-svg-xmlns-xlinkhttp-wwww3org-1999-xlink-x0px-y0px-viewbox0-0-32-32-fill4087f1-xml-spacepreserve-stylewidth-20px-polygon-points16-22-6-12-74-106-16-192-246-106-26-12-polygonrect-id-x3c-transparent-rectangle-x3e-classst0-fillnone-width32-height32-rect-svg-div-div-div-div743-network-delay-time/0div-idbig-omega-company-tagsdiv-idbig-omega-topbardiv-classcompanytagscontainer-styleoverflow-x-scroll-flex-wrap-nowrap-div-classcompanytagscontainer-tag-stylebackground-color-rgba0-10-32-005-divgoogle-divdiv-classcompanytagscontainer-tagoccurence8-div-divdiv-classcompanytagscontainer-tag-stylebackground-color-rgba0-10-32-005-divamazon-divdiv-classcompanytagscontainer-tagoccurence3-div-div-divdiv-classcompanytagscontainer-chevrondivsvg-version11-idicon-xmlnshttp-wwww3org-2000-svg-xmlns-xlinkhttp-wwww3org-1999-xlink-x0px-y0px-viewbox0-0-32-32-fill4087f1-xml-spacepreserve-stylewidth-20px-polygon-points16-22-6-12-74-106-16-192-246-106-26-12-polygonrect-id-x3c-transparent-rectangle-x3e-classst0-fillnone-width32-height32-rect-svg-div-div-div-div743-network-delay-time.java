9b3bbf30b713c6df4fd52892c8be69cd81a3d3c2
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2023-12-26 11:23:08 +0530

    Time: 13 ms (73.71%), Space: 49.6 MB (5.35%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div743-network-delay-time/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div743-network-delay-time.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div743-network-delay-time/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div743-network-delay-time.java
@@ -1,65 +1,37 @@
-class Pair {
-    int node;
-    int time;
-    
-    Pair(int node, int time) {
-        this.node = node;
-        this.time = time;
-    }
-}
 class Solution {
+    //bellman-ford
     public int networkDelayTime(int[][] times, int n, int k) {
-        
-        ArrayList<ArrayList<Pair>> adjList = new ArrayList<>();
-        
-        for(int i = 0; i <=n; i++) {
-           adjList.add(new ArrayList<>());
-        }
-        
-        for(int[] time : times) {
-            int u = time[0];
-            int v = time[1];
-            int t = time[2];
-            adjList.get(u).add(new Pair(v,t));
-        }
-        
-        //vertex starting from index 1
+        //stores the shortest distnace of all nodes from the given src
         int[] distance = new int[n+1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y) -> x.time - y.time);
-        pq.add(new Pair(k, 0));
-        distance[k] = 0;
+        distance[k] = 0; 
         
-        while(pq.size() > 0) {
-            Pair it = pq.poll();
-            int node = it.node;
-            int time = it.time; // time required to reach the node from the source node
-            
-            for(Pair neighbor : adjList.get(node)) {
-                int adjNode = neighbor.node;
-                int edgeW = neighbor.time;
+        //relaxing all the edges (n - 1) times
+        for(int i = 1; i <= n; i++) {
+            for(int[] time : times) {
+                int u = time[0];
+                int v = time[1];
+                int t = time[2];
                 
-                if(distance[adjNode] > distance[node] + edgeW) {
-                    distance[adjNode] = distance[node] + edgeW;
-                    pq.add(new Pair(adjNode,distance[adjNode]) );
+                if(distance[u] != Integer.MAX_VALUE && distance[v] > distance[u] + t) {
+                    distance[v] = distance[u] + t;  //relaxation
                 }
             }
         }
         
-        //any node with distnace value Integer.MAX_VALUE, that means that node is nkot reachable from the given src
         for(int i = 1; i <= n; i++) {
-            if(distance[i] == Integer.MAX_VALUE){
+            if(distance[i] == Integer.MAX_VALUE)
                 return -1;
-            }
         }
-        //max time to reach all the nodes
+        
         int maxTime = -1;
-        for(int i = 1; i <= n; i++){
-            maxTime = Math.max(distance[i], maxTime);
+        for(int i = 1; i <= n; i++) {
+           maxTime = Math.max(maxTime, distance[i]);
         }
         
         return maxTime;
+        
+        
     }
-
 }
