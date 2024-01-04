b00b163b722da1e777089c85dd6d484e3d59084b
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2024-01-04 23:39:52 +0530

    Time: 119 ms (77.59%), Space: 69.8 MB (52.18%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2812-find-the-safest-path-in-a-grid/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2812-find-the-safest-path-in-a-grid.java
@@ -0,0 +1,123 @@
+class Pair {
+    int row;
+    int col;
+    int dist;
+    Pair(int row, int col, int dist) {
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
+    }
+}
+class Solution {
+    
+    private int findSafenessFactor(int[][] distance, int n) {
+        int[][] visited = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                visited[i][j] = 0;
+            }
+        }
+        
+        int[] dx = new int[] {0,0,1,-1};
+        int[] dy = new int[] {1,-1,0,0};
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.dist - a.dist);
+        int ans = n+1;
+        
+        pq.add(new Pair(0,0,distance[0][0]));
+        visited[0][0] = 1;
+        
+        while(pq.size() > 0) {
+            Pair p = pq.peek();
+            pq.remove(); 
+            int cRow = p.row, cCol = p.col, cDist = p.dist;
+            ans = Math.min(ans, p.dist);
+            
+            if(cRow == n-1 && cCol == n-1)
+                break;
+            
+            for(int i = 0; i < 4; i++) {
+                int nr = cRow + dx[i];
+                int nc = cCol + dy[i];
+                
+                if(nr >=0 && nr < n && nc >= 0 && nc < n && visited[nr][nc] == 0) {
+                    visited[nr][nc] = 1;
+                    pq.add(new Pair(nr, nc, distance[nr][nc]));
+                }
+            
+            }
+            
+            
+        }
+        return ans;
+        
+    }
+    
+    private void buildDistanceMatrix( int[][] distance, int[][] visited,  Queue<Pair> queue, int n) {
+        
+        int[] dx = new int[] {0,0,1,-1};
+        int[] dy = new int[] {1,-1,0,0};
+        
+        while(queue.size() > 0) {
+            Pair p = queue.peek();
+            queue.remove();
+            
+            int cRow = p.row, cCol = p.col, cDist = p.dist;
+            
+            for(int i = 0; i < 4; i++) {
+                int nr = cRow + dx[i];
+                int nc = cCol + dy[i];
+                
+                if(nr >=0 && nr < n && nc >= 0 && nc < n && visited[nr][nc] == 0) {
+                    distance[nr][nc] = cDist + 1;
+                    visited[nr][nc] = 1;
+                    queue.add(new Pair(nr, nc, cDist + 1));
+                }
+            }
+        }
+        
+    }
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int n = grid.size();
+        
+        //base case
+        if(grid.get(0).get(0) == 1 || grid.get(n-1).get(n-1) == 1)
+            return 0;
+        
+        
+        int[][] visited = new int[n][n];
+        int[][] distance = new int[n][n];
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                visited[i][j] = 0;
+                distance[i][j] = n+1;
+            }
+        }
+        
+        Queue<Pair> queue = new LinkedList<>();
+        
+        //adding all the theif cells to the queue
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid.get(i).get(j) == 1) {
+                    queue.add(new Pair(i, j, 0));
+                    visited[i][j] = 1;
+                    distance[i][j] = 0;
+                }
+            }
+        }
+        
+        //building a distance array where each cell respresents distance of that cell with the nearest theif
+        buildDistanceMatrix(distance, visited, queue, n);
+        
+        /* for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(distance[i][j] + " " );
+            }
+            System.out.println();
+        } */
+        
+       return findSafenessFactor(distance, n);
+    }
+}
