ed931b823693d42c877eb262cfa37597c9dc7d1c
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2024-01-07 18:52:27 +0530

    Time: 5 ms (46.06%), Space: 41.4 MB (43.90%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div97-interleaving-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div97-interleaving-string.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div97-interleaving-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div97-interleaving-string.java
@@ -1,45 +1,48 @@
 class Solution {
-    //Recursive Approach + Memoization
-    private boolean isInterleaveUtils(String s1, int idx1, String s2, int idx2, String s3, int idx3, Boolean[][] dp) {
-        if(idx1 == s1.length()-1 && idx2 == s2.length()-1 && idx3 == s3.length()-1)
-            return true;
-        
-        //s1 reached the limit, check if remaning characters of s2 matches with the remaning characters of s3 
-        if(idx1 >= s1.length()) return s2.substring(idx2).equals(s3.substring(idx3));
-        
-        //s2 reached the limit, check if remaning characters of s1 matches with the remaning characters of s3 
-        if(idx2 >= s2.length()) return s1.substring(idx1).equals(s3.substring(idx3));
-        
-        if(dp[idx1][idx2] != null){
-            return dp[idx1][idx2];
-        }
-        
-        boolean ans = false;
-        
-        if(s1.charAt(idx1) == s3.charAt(idx3)) 
-           ans = isInterleaveUtils(s1, idx1+1, s2, idx2, s3, idx3+1, dp);
-        
-          
-        if(s2.charAt(idx2) == s3.charAt(idx3)) 
-            ans = ans || isInterleaveUtils(s1, idx1, s2, idx2+1, s3, idx3+1, dp);
-        
-        return dp[idx1][idx2] = ans;
-    }
+ 
     public boolean isInterleave(String s1, String s2, String s3) {
         int n = s1.length();
         int m = s2.length();
-        int k = s3.length();
+        int l = s3.length();
         
-        if(n== 0 && m == 0 && k == 0) return true;
+        if(n== 0 && m == 0 && l == 0) return true;
         
         //if length of s1 + s2 is not equal to length of s3
-        if(n+m != k) return false;
+        if(n+m != l) return false;
         
-        //defalut value is null
-        Boolean[][] dp = new Boolean[s1.length()][s2.length()];
+        //dp[i][j] = true; //means, s1[0..i] and s2[0...j] interleaves with s3 till (i+j)
+        boolean[][] dp = new boolean[n+1][m+1];
         
+        for(int i = 0; i < n+1; i++) {
+            for(int j = 0; j < m+1; j++)
+                dp[i][j] = false;
+        }
         
-        return isInterleaveUtils(s1,0,s2,0,s3,0, dp);
+        //length of s1 = 0, s2 = 0 and s3 = 0 + 0, i.e. 0 . so this will always be true
+        dp[0][0] = true;
         
+        //When s1 is of 0 length, we can interleave only when s2 forms s3
+        for(int i = 1; i < m+1; i++) {
+            dp[0][i] = (s2.charAt(i-1) == s3.charAt(i-1) ? dp[0][i-1] : false);
+        }
+        
+         //When s2 is of 0 length, we can interleave only when s1 forms s3
+        for(int i = 1; i < n+1; i++) {
+            dp[i][0] = (s1.charAt(i-1) == s3.charAt(i-1) ? dp[i-1][0] : false);
+        }
+        
+        for(int i = 1; i < n+1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                int k = i + j - 1;  //length s1 = 1, s2 = j, s3 = i + j ..
+                
+                if(s1.charAt(i-1) == s3.charAt(k)) 
+                    dp[i][j] = dp[i-1][j];
+                
+                if(s2.charAt(j-1) == s3.charAt(k))
+                     dp[i][j] = dp[i][j] || dp[i][j-1];
+            }
+        }
+        
+        return dp[n][m];
     }
 }
