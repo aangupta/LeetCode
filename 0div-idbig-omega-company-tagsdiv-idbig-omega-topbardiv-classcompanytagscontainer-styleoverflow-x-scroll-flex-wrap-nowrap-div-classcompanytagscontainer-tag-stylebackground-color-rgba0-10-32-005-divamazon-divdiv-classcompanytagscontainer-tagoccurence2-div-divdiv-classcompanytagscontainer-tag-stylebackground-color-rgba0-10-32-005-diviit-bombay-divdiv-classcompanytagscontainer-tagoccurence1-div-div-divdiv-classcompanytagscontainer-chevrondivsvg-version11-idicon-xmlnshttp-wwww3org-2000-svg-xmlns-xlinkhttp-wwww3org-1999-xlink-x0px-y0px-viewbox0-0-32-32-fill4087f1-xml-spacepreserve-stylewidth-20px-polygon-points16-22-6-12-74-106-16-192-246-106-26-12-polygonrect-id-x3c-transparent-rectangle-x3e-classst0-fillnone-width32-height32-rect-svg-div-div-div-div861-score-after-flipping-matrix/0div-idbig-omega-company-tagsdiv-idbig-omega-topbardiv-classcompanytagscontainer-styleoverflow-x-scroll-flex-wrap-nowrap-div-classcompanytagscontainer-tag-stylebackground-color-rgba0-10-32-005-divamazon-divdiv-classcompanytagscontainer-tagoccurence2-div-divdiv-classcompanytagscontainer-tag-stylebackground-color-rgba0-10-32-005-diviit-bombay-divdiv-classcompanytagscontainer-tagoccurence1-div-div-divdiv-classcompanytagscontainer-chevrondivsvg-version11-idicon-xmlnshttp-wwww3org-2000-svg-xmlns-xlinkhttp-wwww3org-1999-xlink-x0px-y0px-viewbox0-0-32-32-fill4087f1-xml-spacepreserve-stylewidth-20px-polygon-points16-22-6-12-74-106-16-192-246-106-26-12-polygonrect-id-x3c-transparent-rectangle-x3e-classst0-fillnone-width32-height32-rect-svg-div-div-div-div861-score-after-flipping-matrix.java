ffd4c1181d77ed224fadc5946ab7ce25759f199e
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2024-05-13 16:57:20 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (44.85%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-diviit-bombay-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div861-score-after-flipping-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-diviit-bombay-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div861-score-after-flipping-matrix.java
@@ -0,0 +1,52 @@
+class Solution {
+    //modified the input array
+    public int matrixScore(int[][] grid) {
+        int noRows = grid.length;
+        int noCols = grid[0].length;
+        
+        //traverse the first col and if the MSB value is zero flip it to 1
+        for(int row = 0; row < noRows; row++) {
+            if(grid[row][0] == 0) {
+                
+                //flip that row
+                for(int col = 0; col < noCols; col++) {
+                    grid[row][col] = 1 - grid[row][col]; //flipping
+                }
+            }
+        }
+        
+        //traverse through col = 1 to noCols and if countZeros > countOnes --> flip that col
+        for(int col = 1; col < noCols; col++) {
+            
+            int countZero = 0;
+            for(int row = 0; row < noRows; row++) {
+                if(grid[row][col] == 0)
+                    countZero++;
+            }
+            
+            int countOne = noRows - countZero;
+            if(countZero > countOne) {
+                //flip that col
+                for(int row = 0; row < noRows; row++) {
+                    grid[row][col] = 1 - grid[row][col];
+                }
+            }
+        }
+        
+        int score = 0;
+        for(int row = 0; row < noRows; row++) {
+            for(int col = 0; col < noCols; col++) {
+                int value = grid[row][col] * (1 << noCols - col - 1);  //(1 << noCols - col - 1) -> place value --> pwoer(2, noCols - col - 1)
+                score += value;
+            }
+        }
+        
+        return score; 
+        
+    }
+}
+
+/*
+TC - O(noRows * noCols)
+SC - O(1)
+*/
