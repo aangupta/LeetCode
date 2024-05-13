73ebad45d398a707696c8e079a834cc7397a3fa2
Aanchal Gupta <77240152+aangupta@users.noreply.github.com>
2024-05-13 21:12:10 +0530

    Time: 2 ms (100.00%), Space: 45.3 MB (63.27%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2373-largest-local-values-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2373-largest-local-values-in-a-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    private int findMax(int[][] grid, int x, int y) {
+        int maxElement = 0;
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                maxElement = Math.max(maxElement, grid[i][j]);
+            }
+        }
+        
+        return maxElement;
+    }
+    public int[][] largestLocal(int[][] grid) {
+        int N = grid.length;
+        
+        int[][] result = new int[N - 2][N - 2];
+        
+        for(int i = 0; i < N - 2; i++){
+            for(int j = 0; j < N - 2; j++) {
+                result[i][j] = findMax(grid, i, j);
+            }
+        }
+        
+        return result;
+        
+    }
+}
